c3d5d9919fd13f249f5b67638308d6a9fb8f49ab
Daniel White <82411748+daniel-white99@users.noreply.github.com>
2022-11-30 11:20:51 -0500

    moving strtuctures over to pytricclient

--- pytrisClient.py
+++ pytrisClient.py
@@ -30,5 +30,54 @@ class Client:
 
 
 #create a new client for the server at serverIP:8888, and open on port 25000 for reciveing
-
     
+# PlayerInfo class used for data encoding and decoding over socket
+# Holds information such as username, ip, and locked positions formatted through
+# a json object
+class PlayerInfo(object):
+    def __init__(self, name, IP, grid, locked_pos = {}):
+        self.username = name
+        self.ip = IP
+        self.locked_pos = []
+        self.game_start = False
+        self.game_end = False
+        self.send_garbage = 0
+
+        # Formatting dictionary into a 1D array for json enc
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if (j, i) in locked_pos:
+                    c = locked_pos[(j,i)]
+                    self.locked_pos.append(c)
+                else:
+                    self.locked_pos.append((0,0,0))
+
+    # Updating the 1D array of locked positions
+    def update(self, grid, locked_pos = {}):
+        counter = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if (j, i) in locked_pos:
+                    c = locked_pos[(j,i)]
+                    self.locked_pos[counter] = (c)
+                else:
+                    self.locked_pos[counter] = (0,0,0)
+                counter += 1
+ 
+    # JSON encoding used to send data to client over socket server
+    def json_enc(self):
+        return json.dumps(self, indent = 4, default = lambda o: o.__dict__)
+
+    # JSON decoding used to recieve opponent data and decode into grid
+    def json_dec(self, grid, opponent):
+        counter = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                c = self.locked_pos[counter]
+                if (c != (0,0,0)):
+                    opponent.locked_pos[(j,i)] = (c)
+                counter += 1
+
+    def send_garbage(self, lines):
+        self.send_garbage = lines
+        return
